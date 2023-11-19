b0b8e3da0d21aef62c0efb65987be9ed56d02050
Lala <lalatmi@code.edu.az>
2023-11-19 21:07:43 +0400

    added loop homework

---
+++ Homework #1 - Loop.cs
@@ -0,0 +1,84 @@
+﻿#region Ilin aylari
+using System.ComponentModel.Design;
+string month = "Salam";
+
+if ((month == "January") || (month == "February") || (month == "December"))
+{
+    Console.WriteLine("Winter");
+}
+
+else
+{
+    if ((month == "March") || (month == "April") || (month == "May"))
+    {
+        Console.WriteLine("Spring");
+    }
+    else if ((month == "June") || (month == "July") || (month == "August"))
+    {
+        Console.WriteLine("Summer");
+    }
+    else if ((month == "September") || (month == "October") || (month == "November"))
+    {
+        Console.WriteLine("Fall");
+
+    }
+    else
+    {
+        Console.WriteLine("Please enter valid month");
+    }
+}
+
+
+#endregion
+
+#region Mertebe
+
+//int num = 5;
+//int count = 0;
+
+//while (num > 0)
+//{
+//    num = num / 10;          
+//    count++;
+//}
+
+//Console.WriteLine(count);
+
+
+// Bu mene en chetin geleni oldu. Internetde axtardim sonra ozum tetbiq etdim. Basha dushdum mentiqi.
+
+
+#endregion
+
+#region Factorial
+
+//int n = 5;
+//int factorial = 1;
+
+
+//if (!(n == 0) && !(n < 0))
+//{
+//    for (int i = factorial; i <= n; i++)
+//    {
+//        factorial = factorial * i;
+//    }
+//    Console.WriteLine(factorial);
+
+//}
+//else if (n == 0)
+//{
+//    Console.WriteLine(factorial);
+
+//}
+
+//else
+//{
+//    Console.WriteLine("Invalid number for factorial calculation");
+//}
+
+
+////Burda 0 factorial =1 ve factorial menfi ola bilmez deye if-le diger shertleri de verdim. Meselemizin shertinde o hallar yox idi
+////Ona gore bilmirem achigi duz etmishem onlari elave etmekle yoxsa yox. Umid edirem okay olar
+
+#endregion
+
